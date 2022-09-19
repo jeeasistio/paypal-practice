5a50e641a1e95157b63c1999dab3bf06de63a98c
jeeasistio <jeeasistio08@gmail.com>
2022-09-19 20:44:00 +0800

    fixed env

--- environment.d.ts
+++ environment.d.ts
@@ -1,8 +1,8 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv extends EnvTypes {
-            NEXT_PAYPAL_CLIENT_ID: string
-            NEXT_PAYPAL_CLIENT_SECRET: string
+            NEXT_PUBLIC_PAYPAL_CLIENT_ID: string
+            PAYPAL_CLIENT_SECRET: string
         }
     }
 }
